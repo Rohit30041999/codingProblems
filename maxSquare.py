95defdd8da46849bc47b72f1ab07268d6c341d32
Kundeti Rohit Chowdary <54351002+Rohit30041999@users.noreply.github.com>
2019-11-24 00:00:59 +0530

    Create maxSquare.py

---
+++ maxSquare.py
@@ -0,0 +1,36 @@
+"""
+Maximum length of a square of 1's in a matrix (Dynamic Programming):>>
+example :
+matrix :       1 1 0 0 1
+               1 1 1 1 0
+               0 1 1 1 1
+               1 1 1 1 1
+    if you can observe. The max square is ==>    1 1 1
+                                                 1 1 1
+                                                 1 1 1
+    so return length of its side i.e., 3.
+"""
+
+def maxSquareLength(mat) :
+  clone = mat.copy()
+  maxLength = -pow(2, 31)
+  for i in range(len(mat)):
+    for j in range(len(mat[0])):
+      if i > 0 and  j > 0:
+        minVal = min(clone[i][j-1], min(clone[i-1][j], clone[i-1][j-1]))
+        clone[i][j] += minVal
+        if maxLength < clone[i][j]:
+          maxLength = clone[i][j]
+  return maxLength
+
+def main():
+  matrix = []
+  n, m = int(input().strip()), int(input().strip())
+  for i in range(n) :
+    temp = []
+    for j in range(m) :
+      temp.append(int(input().strip()))
+    matrix.append(temp)
+  print(maxSquareLength(matrix))
+
+main()  
