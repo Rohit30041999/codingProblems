2b63884a069a1c8b564701675a0b923db865b0c7
Kundeti Rohit Chowdary <54351002+Rohit30041999@users.noreply.github.com>
2019-11-24 01:42:16 +0530

    Update maxSquare.py

--- maxSquare.py
+++ maxSquare.py
@@ -13,14 +13,16 @@ matrix :       1 1 0 0 1
 
 def maxSquareLength(mat) :
   clone = mat.copy()
-  maxLength = -pow(2, 31)
+  maxLength = 0
   for i in range(len(mat)):
     for j in range(len(mat[0])):
-      if i > 0 and  j > 0:
+      if i > 0 and  j > 0 and clone[i][j] != 0:
         minVal = min(clone[i][j-1], min(clone[i-1][j], clone[i-1][j-1]))
         clone[i][j] += minVal
         if maxLength < clone[i][j]:
           maxLength = clone[i][j]
+      if (i == 0 and j > 0 and clone[i][j] == 1) or (i > 0 and j == 0 and clone[i][j] == 1):
+        maxLength = clone[i][j]
   return maxLength
 
 def main():
