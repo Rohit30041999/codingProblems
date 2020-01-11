f8782955f2ca4d757b4bdc584043e2bb9f11a0d6
Kundeti Rohit Chowdary <54351002+Rohit30041999@users.noreply.github.com>
2020-01-11 23:38:43 +0530

    Create sortLinkedList.py

---
+++ sortLinkedList.py
@@ -0,0 +1,19 @@
+'''
+Problem Statement:>>> --> 'https://leetcode.com/problems/sort-list/' --> (link to the problem discription)
+'''
+
+#Solution:>>>
+
+def sortList(self, head: ListNode) -> ListNode:
+	if not head or not head.next: return head
+	numbers = []
+   	current = head
+   	while current:
+    	numbers.append(current.val)
+        current = current.next
+    numbers.sort()
+    current = head
+    for num in numbers:
+    	current.val = num
+        current = current.next
+    return head
