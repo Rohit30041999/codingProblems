6716f68d9e556ca65a995846cf043bedc24b7de5
Kundeti Rohit Chowdary <54351002+Rohit30041999@users.noreply.github.com>
2020-01-11 23:39:25 +0530

    Update sortLinkedList.py

--- sortLinkedList.py
+++ sortLinkedList.py
@@ -9,11 +9,11 @@ def sortList(self, head: ListNode) -> ListNode:
 	numbers = []
    	current = head
    	while current:
-    	numbers.append(current.val)
-        current = current.next
-    numbers.sort()
-    current = head
-    for num in numbers:
-    	current.val = num
-        current = current.next
-    return head
+    		numbers.append(current.val)
+        	current = current.next
+    	numbers.sort()
+    	current = head
+    	for num in numbers:
+    		current.val = num
+        	current = current.next
+    	return head
