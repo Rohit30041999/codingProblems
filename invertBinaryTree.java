f8f0fdac961b198d1e1b0d15c9d49d618eee90a5
Kundeti Rohit Chowdary <54351002+Rohit30041999@users.noreply.github.com>
2020-06-01 21:49:29 +0530

    Create invertBinaryTree.java

---
+++ invertBinaryTree.java
@@ -0,0 +1,40 @@
+# (Source --> LeetCode)
+
+# Solution: (Method - I : Recursion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+        // if null return that null root.
+        if(root == null) return root;
+        
+        // store back the right sub tree or node.
+        root.right = invertTree(root.right);
+        
+        // store back the left sub tree or node.
+        root.left = invertTree(root.left);
+        
+        // Swap the nodes.
+        TreeNode tempNode = root.right;
+        root.right = root.left;
+        root.left = tempNode;
+        
+        // return the main root node.
+        return root;
+    }
+}
